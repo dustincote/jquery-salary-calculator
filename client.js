cfcf04dc34cab2e83ba5e99b908f1ed7806583e9
Dustin Cote <dustincote@hotmail.com>
2020-09-11 20:13:34 -0500

    append to table working

--- client.js
+++ client.js
@@ -1,6 +1,9 @@
 console.log("js");
 $().ready(readyNow);
 
+let totalMonthly = 0;//setting global variable to reference for the total monthly calculation
+
+
 function readyNow(){
     console.log("jquery linked and ready");
 // set up what my page is going to look like
@@ -25,9 +28,35 @@ $('body').append(`
             <th>ID</th>
             <th>Title</th>
             <th>Annual Salary</th>
+            <th> </th>
         </tr>
     </thead>
+    <tbody id="employeeTable">
+    </tbody>
 </table>
 
 `);
+
+//click handler for the submit button that call addToTable
+$('#submitButton').on('click',addToTable);
+}
+
+
+
+function addToTable(){
+    //setting inputs to variables to make them easier to work with
+    let fName = $('#fNameInput').val();
+    let lName = $('#lNameInput').val();
+    let id = $('#idInput').val();
+    let title = $('#titleInput').val();
+    let annualSalary = $('#annualSalaryInput').val();
+    //console.log(`${fName},${lName},${id},${title},${annualSalary}`);//loging the variables to test inputs before passing into dom
+    //append the table with the inputs
+    $('#employeeTable').append(`
+    <tr>
+        <td>${fName}</td><td>${lName}</td><td>${id}</td><td>${title}</td><td>${annualSalary}</td><td><button class="deleteRow">Delete</button></td>
+    </tr>
+    `)
+    totalMonthly += Number(annualSalary);
+    console.log(totalMonthly);
 }
